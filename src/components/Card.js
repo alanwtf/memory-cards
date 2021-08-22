1ca60a48f9f05c3b1fadabf49a6853acb1e4eaec
ALAN MATHIASEN <alan.mathiasen@outlook.com>
2021-08-22 19:24:30 -0300

    First commit

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={props.jotaro} alt="" />
+      <footer>JOTARO JOSUKE</footer>
+    </div>
+  );
+}
+
+export default Card;
