ebd02184f2abb8b5a63ec720be8653c0c813b83f
ALAN MATHIASEN <alan.mathiasen@outlook.com>
2021-08-28 22:30:37 -0300

    added game logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,108 @@
+import React, { useState } from 'react';
 import jotaro from './img/jotaro3.png';
 import './App.css';
 import Card from './components/Card';
+
 function App() {
+  const [score, setScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
+  const [cards, setCards] = useState([
+    {
+      hasBeenClicked: false,
+      src: jotaro,
+      id: 0,
+    },
+    {
+      hasBeenClicked: false,
+      src: jotaro,
+      id: 1,
+    },
+    {
+      hasBeenClicked: false,
+      src: jotaro,
+      id: 2,
+    },
+    {
+      hasBeenClicked: false,
+      src: jotaro,
+      id: 3,
+    },
+    {
+      hasBeenClicked: false,
+      src: jotaro,
+      id: 4,
+    },
+    {
+      hasBeenClicked: false,
+      src: jotaro,
+      id: 5,
+    },
+  ]);
+  function increaseScore() {
+    setScore(score + 1);
+    if (maxScore <= score) {
+      setMaxScore(score + 1);
+    }
+  }
+
+  function checkClicks(id) {
+    if (cards[id].hasBeenClicked) {
+      alert('YOUY LOSE YOU FAUCKING IDIOT');
+      reset();
+    } else {
+      console.log(id);
+      increaseScore();
+      setCards(
+        cards.map((singular) => {
+          if (singular.id === id) {
+            return {
+              ...singular,
+              hasBeenClicked: true,
+            };
+          } else {
+            return singular;
+          }
+        })
+      );
+    }
+  }
+
+  function shuffleCards() {
+    setCards(cards.sort(() => Math.random() - 0.5));
+    console.log(cards);
+  }
+
+  function reset() {
+    setCards(cards.sort(() => Math.random() - 0.5));
+    setCards(
+      cards.map((singular) => {
+        return {
+          ...singular,
+          hasBeenClicked: false,
+        };
+      })
+    );
+    shuffleCards();
+    setScore(0);
+  }
+
   return (
-    <div className="App">
-      <Card jotaro={jotaro} />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div>
+      <div>
+        Score: {score} Max Score: {maxScore}
+      </div>
+      <div className="App">
+        {cards.map((card) => {
+          return (
+            <Card
+              key={card.id}
+              info={card}
+              add={increaseScore}
+              checkClicks={checkClicks}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,16 @@
+import React, { useState } from 'react';
+
 function Card(props) {
+  const [clickedOnce, setClickedOnce] = useState(false);
+
+  function cardClicked() {
+    setClickedOnce(true);
+  }
+
   return (
-    <div className="card">
-      <img src={props.jotaro} alt="" />
-      <footer>JOTARO JOSUKE</footer>
+    <div className="card" onClick={() => props.checkClicks(props.info.id)}>
+      <img src={props.info.src} alt="" />
+      <footer>JOTARO JOSUKE{props.info.id}</footer>
     </div>
   );
 }
